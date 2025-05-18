28f52bc1909f4300ba3f2dfb41021a9937cb9387
Bernkra96git config --global user.name Bernkra96 <bernkra@gmail.com>
2025-05-18 19:05:56 +0200

    Fix stig pint bug and add end page :

--- fibonacci-python.py
+++ fibonacci-python.py
@@ -1,11 +1,14 @@
 # Fibonacci sequence
+import sys
 
 A = 0  # Start Val A B 
 B = 1
 
-runFor = 10 #nOfRuns
+runFor = 500 #nOfRuns
 
+limString = 0 #set limit for int to string conversion // 0 for unlimited
 
+sys.set_int_max_str_digits(limString)
 
 for i in range(runFor):
      
@@ -15,3 +18,6 @@ for i in range(runFor):
      print(C)
      B = A
      A = C
+print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+print('Set Number of Runs:',runFor)
+print('Run Number of Runs:',i+1)
