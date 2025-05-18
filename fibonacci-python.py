0fbff2a9a626e08a10092b26d15b22b76b6772e0
Bernkra96git config --global user.name Bernkra96 <bernkra@gmail.com>
2025-05-18 16:15:24 +0200

    Set UP and first Commit :

---
+++ fibonacci-python.py
@@ -0,0 +1,17 @@
+# Fibonacci sequence
+
+A = 0  # Start Val A B 
+B = 1
+
+runFor = 10 #nOfRuns
+
+
+
+for i in range(runFor):
+     
+     C = A + B
+   
+       
+     print(C)
+     B = A
+     A = C
