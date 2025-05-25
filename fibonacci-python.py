877c03af8d1c5194d7dab2af85059f6f3d761d76
Bernkra96git config --global user.name Bernkra96 <bernkra@gmail.com>
2025-05-25 19:08:14 +0200

    Update End Print

--- fibonacci-python.py
+++ fibonacci-python.py
@@ -6,11 +6,12 @@ import time
 A = 0  # Start Val A B 
 B = 1
 
-runFor = 5000 #nOfRuns
+runFor = 10000 #nOfRuns
 
 limString = 0 #set limit for int to string conversion // 0 for unlimited
 
 sys.set_int_max_str_digits(limString)
+
 startTime = time.time() #Start Time 
 
 for i in range(runFor):
@@ -21,9 +22,13 @@ for i in range(runFor):
      print(C)
      B = A
      A = C
+
 endTime = time.time()      #END Time 
-print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-print('Set Number of Runs:',runFor)
-print('Run Number of Runs:',i+1)
-print('Start Time',startTime,'End Time',endTime)
-print('Run Time',endTime - startTime)
+print('┌─────────────────────────────────────────────────────────')
+print('│','Set Number of Runs:',runFor,)
+print('│','Run Number of Runs:',i+1,)
+print('├─────────────────────────────────────────────────────────')
+print('│','Start Time',startTime,'End Time',endTime,)
+print('│','Run Time seconds',endTime - startTime,)
+print('└─────────────────────────────────────────────────────────')
+time.sleep(0)
