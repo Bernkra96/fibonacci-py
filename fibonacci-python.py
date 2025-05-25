980fe612cd86c23236b186add2cee68c093df7a2
Bernkra96git config --global user.name Bernkra96 <bernkra@gmail.com>
2025-05-25 16:30:39 +0200

    add run Time

--- fibonacci-python.py
+++ fibonacci-python.py
@@ -1,14 +1,17 @@
 # Fibonacci sequence
 import sys
+import time
+
 
 A = 0  # Start Val A B 
 B = 1
 
-runFor = 500 #nOfRuns
+runFor = 5000 #nOfRuns
 
 limString = 0 #set limit for int to string conversion // 0 for unlimited
 
 sys.set_int_max_str_digits(limString)
+startTime = time.time() #Start Time 
 
 for i in range(runFor):
      
@@ -18,6 +21,9 @@ for i in range(runFor):
      print(C)
      B = A
      A = C
+endTime = time.time()      #END Time 
 print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
 print('Set Number of Runs:',runFor)
 print('Run Number of Runs:',i+1)
+print('Start Time',startTime,'End Time',endTime)
+print('Run Time',endTime - startTime)
