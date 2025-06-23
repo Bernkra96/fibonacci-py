b6ec9c3f8e907235ca3dbcab34fc5e5385957fdb
Bernkra96git config --global user.name Bernkra96 <bernkra@gmail.com>
2025-06-23 16:12:03 +0200

    Fix sapcing

--- fibonacci-python.py
+++ fibonacci-python.py
@@ -6,29 +6,60 @@ import time
 A = 0  # Start Val A B 
 B = 1
 
-runFor = 10000 #nOfRuns
-
+runFor = 1111 #nOfRuns
 limString = 0 #set limit for int to string conversion // 0 for unlimited
 
-sys.set_int_max_str_digits(limString)
+def setup():
+     print('Hallo Welcome')
 
-startTime = time.time() #Start Time 
+     print('Runs Fibonacci for',runFor,'Numbers')
 
-for i in range(runFor):
+
+
+
+def calc():  
+     global A
+     global B
      
-     C = A + B
-   
+     global i
+     global startTime
+     global endTime
+     startTime = time.time() #Start Time 
+     for  i in range(runFor):
+     
+          C = A + B
        
-     print(C)
-     B = A
-     A = C
+          print(C)
+          B = A
+          A = C
 
-endTime = time.time()      #END Time 
-print('┌─────────────────────────────────────────────────────────')
-print('│','Set Number of Runs:',runFor,)
-print('│','Run Number of Runs:',i+1,)
-print('├─────────────────────────────────────────────────────────')
-print('│','Start Time',startTime,'End Time',endTime,)
-print('│','Run Time seconds',endTime - startTime,)
-print('└─────────────────────────────────────────────────────────')
-time.sleep(0)
+
+     endTime = time.time() 
+
+ #END Time 
+
+
+def printResult(): 
+      
+     if 'i' in globals() or 'i' in locals() :
+        
+
+          print('┌─────────────────────────────────────────────────────────'),
+          print('│','Set Number of Runs:',runFor,),
+          print('│','Run Number of Runs:',i+1,),
+          print('├─────────────────────────────────────────────────────────'),
+          print('│','Start Time',startTime,'End Time',endTime,),
+          print('│','Run Time seconds',endTime - startTime,),
+          print('└─────────────────────────────────────────────────────────')
+     elif  runFor == 0 : 
+          print('Error No Run. Run for set to wars Set to 0')
+     else: 
+          print('Error No Run. ')
+
+     
+    
+
+setup()
+calc()
+printResult()    
+time.sleep(10)
