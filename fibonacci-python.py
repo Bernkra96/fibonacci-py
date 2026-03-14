6229568024bb79a909da71afee0cf81f38fc5700
Bernkra96git config --global user.name Bernkra96 <bernkra@gmail.com>
2026-03-14 14:34:18 +0100

    UPATTE add Wile loobs ro unserinput & add Nav optons :

--- fibonacci-python.py
+++ fibonacci-python.py
@@ -24,46 +24,35 @@ def setup():
      print('To this Fibonacci Runner / Benchmark.')
      print('Set Number off runs.')
      print("o for options, s for set up ") 
-     
-     inputVal = input("Number or text(Listed Only).: ")
+
+     while True:
+          inputVal = input("Number or text(Listed Only).: ")
      
     
-     if   any(char.isdigit() for char in inputVal) is True:
+          if   any(char.isdigit() for char in inputVal) is True:
            print("Is Valid InT.") 
            inputVal = int(inputVal)
-     elif any(char.lower() == 'o' for char in inputVal):
-
-          options()     
-     elif any(char.lower() == 's' for char in inputVal):
-          for x in optionsDataSet:
            
-           updateSetting(optionsDataSet , x )    
-
+           runFor = inputVal
+           print(f'Runs Fibonacci.Set for {runFor} times.')
+           break
            
-          setup()     
-     else :
+          elif any(char.lower() == 'o' for char in inputVal):
+
+                options()    
+                break 
+          elif any(char.lower() == 's' for char in inputVal):
+           
+           print(optionsDataSet)   
+           
+           setup()  
+           break
+           
+          else :
 
            print("Is not Valid InT.")     
-           sys.exit()
+           
           
-      
-      
-   
-     
-     
-
-     
-
-     
-
-    
-    
-    
-    
-     runFor = inputVal
-     print(f'Runs Fibonacci.Set for {runFor} times.')
-   
-     
 def calc():  
      global A
      global B
@@ -83,7 +72,6 @@ def calc():
      endTime = time.time() 
  
  #END Time 
-
 
 
 def printResult(): 
@@ -107,74 +95,77 @@ def printResult():
 def options():
      print("Options") 
      print("e for exit,h for help,i for info,u for update") 
-     inputVal = input("Text(Listed Only).: ")
-     
-     if any(char.lower() == 'e' for char in inputVal):
-          setup()
-     elif  any(char.lower() == 'h' for char in inputVal):
-          print('Help')  
-          print('o = Options')  
-          print('Opens Options ')
-          print('e = Exit ')  
-          print('Exit Options ')  
-          print('u = Exit ')  
-          print(f'Update Options')    
-          options()   
-     elif  any(char.lower() == 'i' for char in inputVal):
-          print('Info')  
-          print(optionsDataSet) 
-          options() 
-     elif  any(char.lower() == 'u' for char in inputVal):
-          print('Update')  
-          updaterOptions()     
-     else :
+    
+     while True:
+          inputVal = input("Text(Listed Only).: ")
+          
+          if any(char.lower() == 'e' for char in inputVal):
+               setup()
+               break
+          elif  any(char.lower() == 'h' for char in inputVal):
+               print('Help')  
+               print('o = Options')  
+               print('Opens Options ')
+               print('e = Exit ')  
+               print('Exit Options ')  
+               print('u = Exit ')  
+               print(f'Update Options')    
+               
+          elif  any(char.lower() == 'i' for char in inputVal):
+               print('Info')  
+               print(optionsDataSet) 
+               
+          elif  any(char.lower() == 'u' for char in inputVal):
+               print('Update')  
+               updaterOptions() 
+               break    
+          else :
 
-          print("Is not Valid ")     
-          sys.exit()
+             print("Is not Valid ")     
+          
           
 
 def updaterOptions():
-    
+     while True:
+               
      
-     print("Update witch Option ?")
-     print(optionsDataSet) 
-     selectOptionsUserInput = input("Text(Option Name).: ")
-   
-     if selectOptionsUserInput in optionsDataSet:
-        updateSetting(optionsDataSet,selectOptionsUserInput)
-     else: 
-          print("No Valet input")
-          options()
+          print("Update witch Option ?")
+          print(optionsDataSet) 
+          print("Put Option Name in or e for exit") 
+
+          selectOptionsUserInput = input("Text(Option Name).: ")
+
+         
+          if selectOptionsUserInput in optionsDataSet:
+               updateSetting(optionsDataSet,selectOptionsUserInput)
+               break
+          elif any(char.lower() == 'e' for char in selectOptionsUserInput):
+               options()
+               break
+          else: 
+           print("No Valet input")
+          
 
         
-     
-     
-     
-   
-     
-     
-     
+def updateSetting(name , keyData ):    
 
-def updateSetting(name , key ):    
-   
+
+     key = keyData
+     
      match name[key]:
           case bool():
               print("Is a Bool Stetting")
               print(f'Name of Setting {key} and is, {name[key]}')
-          
+              
           case int():
               print("Is Int Setting")
               print(f'Name of Setting {key} and is, {name[key]}')
           case _:
                print("Error No Valid Format ")
+     options()        
  
-     options() 
-
      
 
-
-          
-     
 
 setup()
 calc()
